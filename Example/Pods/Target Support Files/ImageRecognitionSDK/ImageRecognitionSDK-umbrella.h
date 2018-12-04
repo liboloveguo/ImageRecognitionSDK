b37dd2a8de25e728dafff581660c4baae1e50a76
张利果 <zhang.liguo@icerno.com>
2018-12-04 16:23:02 +0800

    再次提交文件

--- Example/Pods/Target Support Files/ImageRecognitionSDK/ImageRecognitionSDK-umbrella.h
+++ Example/Pods/Target Support Files/ImageRecognitionSDK/ImageRecognitionSDK-umbrella.h
@@ -10,6 +10,18 @@
 #endif
 #endif
 
+#import "ImageRecognition.h"
+#import "AipBase.h"
+#import "AipOpenUDID.h"
+#import "AipTokenManager.h"
+#import "NSErrorHelper.h"
+#import "AipCaptureCardVC.h"
+#import "AipGeneralVC.h"
+#import "AipOcrSdk.h"
+#import "AipOcrService.h"
+#import "IdcardQuality.h"
+#import "IdcardQualityAdaptor.h"
+#import "idcard_quality.h"
 
 FOUNDATION_EXPORT double ImageRecognitionSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char ImageRecognitionSDKVersionString[];
